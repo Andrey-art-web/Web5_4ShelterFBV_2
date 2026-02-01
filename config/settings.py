a77cfbf2a47417cb1beeff9b8a62fda6e5a88ac6
Andrey Maltcev <andreyalexsandrowich@yandex.ru>
2026-02-02 00:58:22 +0300

    Добавлена модель пользователя в Админку внесены необходимые изменения в настройки

--- config/settings.py
+++ config/settings.py
@@ -81,6 +81,7 @@ USER = os.getenv('MS_SQL_USER')
 PASSWORD = os.getenv('MS_SQL_KEY')
 HOST = os.getenv('MS_SQL_SERVER')
 DATABASE = os.getenv('MS_SQL_DATABASE')
+PAD_DATABASE = os.getenv('MS_SQL_PAD_DATABASE')
 DRIVER = os.getenv('MS_SQL_DRIVER')
 
 # DATABASES = {
@@ -127,9 +128,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC+3'
 
 USE_I18N = True
 
@@ -145,3 +146,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'
